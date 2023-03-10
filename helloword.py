26149b8a8841eda6d2e582dbb1441bd027d2a85e
Ilya Soroka <ilycha92@gmail.com>
2023-03-09 20:52:21 -0600

    First commit

---
+++ helloword.py
@@ -0,0 +1,18 @@
+x = 3
+y = 3
+
+z = x ** y
+print(z)
+
+hours = 40.0
+rate = 30
+weeks = 4
+pay = weeks * hours * rate
+print(pay)
+
+nam = input('Who are you? ')
+print('Welcome', nam)
+
+inp = input('Europe floor?')
+usf = int(inp)
+print('Floor is', usf)
